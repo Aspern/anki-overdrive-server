670d95a8b683e5fbac2ddbe211e22a82350ee8bb
xgxtadw <adrian.weber@msg.group>
2018-10-26 15:51:45 +0200

    Updated kafka settings

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,5 +1,5 @@
 class Settings {
-  public static kafkaHost = '192.168.188.25:9092'
+  public static kafkaHost = 'localhost:9092'
   public static webServerPort = 8090
   public static vehicleNames = new Map([
       ['ed0c94216553', 'Skull'],
