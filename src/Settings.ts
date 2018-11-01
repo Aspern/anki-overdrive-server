48893d818826845a9b39b0680af4cc8e6d8da97f
xgxtadw <adrian.weber@msg.group>
2018-11-01 09:48:58 +0100

    Corrected kafka topic name

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,6 +1,6 @@
 class Settings {
   public static kafkaHost = 'localhost:9092'
-  public static kafkaTopic = 'vehicle_messages'
+  public static kafkaTopic = 'vehicle_message'
   public static webServerPort = 8090
   public static vehicleNames = new Map([
       ['ed0c94216553', 'Skull'],
